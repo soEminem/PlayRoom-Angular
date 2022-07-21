4a1c73b53a0b0fdef4b1c528e9ffae68cc20e89f
RaucciAngelo <raucciiangeloo@gmail.com>
2022-07-21 17:14:52 +0200

    Last

--- angular8-springboot-basic-auth-login-logout-master/angular-login-logout/src/app/login/login.component.ts
+++ angular8-springboot-basic-auth-login-logout-master/angular-login-logout/src/app/login/login.component.ts
@@ -11,7 +11,7 @@ export class LoginComponent implements OnInit {
 
   username: string;
   password : string;
-  errorMessage = 'Invalid Credentials';
+  errorMessage = 'Invalid Credentials'; //I
   successMessage: string;
   invalidLogin = false;
   loginSuccess = false;
